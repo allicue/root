ae51f3c5e817eeb8759ab61b189da3a333972c3a
Eddie <edward.f.didonato@gmail.com>
2020-10-29 17:53:47 -0400

    position transportation types with flexbox

--- client/src/components/TransportationType/TransportationType.jsx
+++ client/src/components/TransportationType/TransportationType.jsx
@@ -5,7 +5,7 @@ export default function TransportationType(props) {
 
 
   return (
-    <div>
+    <div className="transportation-type">
       <div className="title-container" style={{ background: `url(${props.imgURL})`, backgroundSize: "300px" }}>
         <h3 >{props.title}</h3>
       </div>
--- client/src/screens/TransportationType/TransportationTypes.jsx
+++ client/src/screens/TransportationType/TransportationTypes.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import TransportationType from '../../components/TransportationType/TransportationType'
+import './TransportationTypes.css'
 import { getTransportationTypes } from '../../services/transportationTypes'
 
 export default function TransportationTypes() {
@@ -21,24 +22,34 @@ export default function TransportationTypes() {
 
   return (
     <div className="Layout-placeholder">
-      <h1>CLIMATE IMPACT BY TRANSPORTATION TYPE</h1>
-      <div className="transportation-types-parent">
-        <div className="transportation-types">{TYPES}</div>
+      <div className="transportation-types-main">
+        <div className="title-list">
+          <div className="transportation-types-parent">
+            <h1>CLIMATE IMPACT BY TRANSPORTATION TYPE</h1>
+            <div className="transportation-types">{TYPES}</div>
+          </div>
+        </div>
         <div className="green-news">
           <div className="green-news-child">
-            <h2>GREEN NEWS</h2>
-            <h4>A GREEN NEW DEAL FOR ILLINOIS TRANSPORTATION</h4>
+            <div className="green-news-title" id="illinois">
+              <h2>GREEN NEWS</h2>
+              <h4>A GREEN NEW DEAL FOR ILLINOIS TRANSPORTATION</h4>
+            </div>
             <small>Last week while forest fires were raging in California, Governor Gavin Newsom issued an executive order declaring a goal of eliminating fossil fuels from the transportation sector.<br></br>
-                  The order called for ending all sales of fossil fuel-powered vehicles by 2035 and calls on various California state agencies to formulate strategies and regulations to create a “just” transition away from fossil fuel reliance.</small>
+                    The order called for ending all sales of fossil fuel-powered vehicles by 2035 and calls on various California state agencies to formulate strategies and regulations to create a “just” transition away from fossil fuel reliance.</small>
           </div>
           <div className="green-news-child">
-            <h2>GREEN NEWS</h2>
-            <h4>AMAZON UNVEILS ITS NEW ELECTRIC DELIVERY VANS BUILT BY RIVIAN</h4>
+            <div className="green-news-title" id="amazon">
+
+              <h2>GREEN NEWS</h2>
+              <h4>AMAZON UNVEILS ITS NEW ELECTRIC DELIVERY VANS BUILT BY RIVIAN</h4>
+            </div>
             <small>Amazon unveiled its first all-electric delivery van on Thursday. The vehicle, built by EV startup Rivian, will come with state-of-the-art technology, like sensing equipment and an advanced driver-assist system. <br></br>
-The e-commerce giant says it expects to have 10,000 vans on the road making deliveries “as early as 2022,” with a total fleet of 100,000 vehicles expected by 2030.</small>
+                    The e-commerce giant says it expects to have 10,000 vans on the road making deliveries “as early as 2022,” with a total fleet of 100,000 vehicles expected by 2030.</small>
           </div>
         </div>
       </div>
+
     </div>
   )
 }
