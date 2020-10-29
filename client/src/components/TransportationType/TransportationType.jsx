f76f99a778b560865459de7033693f36bf4b3f07
Eddie <edward.f.didonato@gmail.com>
2020-10-29 16:56:28 -0400

    add green news pieces

--- client/src/components/TransportationType/TransportationType.jsx
+++ client/src/components/TransportationType/TransportationType.jsx
@@ -6,8 +6,9 @@ export default function TransportationType(props) {
 
   return (
     <div>
-      <div className="title-container" style={{ background: `url(${props.imgURL})`, backgroundSize: "300px" }}><h1 >{props.title}</h1></div>
-      {/* <img src={props.imgURL} /> */}
+      <div className="title-container" style={{ background: `url(${props.imgURL})`, backgroundSize: "300px" }}>
+        <h3 >{props.title}</h3>
+      </div>
       <p>{props.description}</p>
     </div>
   )
--- client/src/screens/TransportationType/TransportationTypes.jsx
+++ client/src/screens/TransportationType/TransportationTypes.jsx
@@ -24,7 +24,20 @@ export default function TransportationTypes() {
       <h1>CLIMATE IMPACT BY TRANSPORTATION TYPE</h1>
       <div className="transportation-types-parent">
         <div className="transportation-types">{TYPES}</div>
-        <div className="green-news"></div>
+        <div className="green-news">
+          <div className="green-news-child">
+            <h2>GREEN NEWS</h2>
+            <h4>A GREEN NEW DEAL FOR ILLINOIS TRANSPORTATION</h4>
+            <small>Last week while forest fires were raging in California, Governor Gavin Newsom issued an executive order declaring a goal of eliminating fossil fuels from the transportation sector.<br></br>
+                  The order called for ending all sales of fossil fuel-powered vehicles by 2035 and calls on various California state agencies to formulate strategies and regulations to create a “just” transition away from fossil fuel reliance.</small>
+          </div>
+          <div className="green-news-child">
+            <h2>GREEN NEWS</h2>
+            <h4>AMAZON UNVEILS ITS NEW ELECTRIC DELIVERY VANS BUILT BY RIVIAN</h4>
+            <small>Amazon unveiled its first all-electric delivery van on Thursday. The vehicle, built by EV startup Rivian, will come with state-of-the-art technology, like sensing equipment and an advanced driver-assist system. <br></br>
+The e-commerce giant says it expects to have 10,000 vans on the road making deliveries “as early as 2022,” with a total fleet of 100,000 vehicles expected by 2030.</small>
+          </div>
+        </div>
       </div>
     </div>
   )
