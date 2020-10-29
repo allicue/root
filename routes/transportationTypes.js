30ad9d766dcab4b9e9b6d75962543e7072d11e25
Daniel Michael <itzdanielmichael@gmail.com>
2020-10-29 15:08:25 -0400

    done with the backend, just need seeds

--- routes/transportationTypes.js
+++ routes/transportationTypes.js
@@ -4,5 +4,9 @@ const controllers = require('../controllers/transportationTypes')
 const router = Router()
 
 router.get('/transportationTypes', controllers.getTransportationTypes)
+router.get('/transportationTypes/:id', controllers.getTransportationType)
+router.post('/transportationTypes', controllers.createTransportationType)
+router.put('/transportationTypes/:id', controllers.updateTransportationType)
+router.delete('/transportationTypes/:id', controllers.deleteTransportationType)
 
 module.exports = router
