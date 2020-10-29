f745f9d8b1636af9781b5c28f4144d74ae42c998
Daniel Michael <itzdanielmichael@gmail.com>
2020-10-29 16:53:09 -0400

    created containers for  top, top left and top right, finished with right input but missing asset image

--- client/src/screens/PoliciesInitiatives/PoliciesInitiatives.jsx
+++ client/src/screens/PoliciesInitiatives/PoliciesInitiatives.jsx
@@ -5,15 +5,17 @@ import './PoliciesInitiatives.css'
 function PoliciesInitiatives() {
   return (
   <>
-    <div className="top-left-container">
+   <div className="top-container">
+      <div className="top-left-container">
       <p className="page-direction">Home/ Policies + Initiatives</p>
       <img className="root-logo" src="https://i.imgur.com/mV0w6j7.png" alt="ROOT Logo"/>
       <h1 className='title'>POLICIES + INITIATIVES</h1>
       </div>
       <div className="top-right-container">
-        <p>Or enter your location:</p>
-        <input type="text" placeholder="Zipcode or City, State" />
-      </div>
+        <p className='enter-location'>Or enter your location:</p>
+        <input className="top-right-input" type="text" placeholder="Zipcode or City, State" />
+        </div>
+        </div>
 </>
   )
 }
