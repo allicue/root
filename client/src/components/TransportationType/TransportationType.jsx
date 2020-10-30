1bce3811a853bf1a132239186f9d4cbd7d74f086
Eddie <edward.f.didonato@gmail.com>
2020-10-29 20:15:04 -0400

    finish baseline page styling

--- client/src/components/TransportationType/TransportationType.jsx
+++ client/src/components/TransportationType/TransportationType.jsx
@@ -6,10 +6,12 @@ export default function TransportationType(props) {
 
   return (
     <div className="transportation-type">
-      <div className="title-container" style={{ background: `url(${props.imgURL})`, backgroundSize: "300px" }}>
+      <div className="title-container" style={{ background: `url(${props.imgURL})`, backgroundSize: "380px", backgroundPosition: "center" }}>
         <h3 >{props.title}</h3>
       </div>
-      <p>{props.description}</p>
+      <div className="description-container">
+        <p>{props.description}</p>
+      </div>
     </div>
   )
 }
--- client/src/screens/TransportationType/TransportationTypes.jsx
+++ client/src/screens/TransportationType/TransportationTypes.jsx
@@ -22,34 +22,41 @@ export default function TransportationTypes() {
 
   return (
     <div className="Layout-placeholder">
-      <div className="transportation-types-main">
-        <div className="title-list">
-          <div className="transportation-types-parent">
-            <h1>CLIMATE IMPACT BY TRANSPORTATION TYPE</h1>
-            <div className="transportation-types">{TYPES}</div>
-          </div>
-        </div>
-        <div className="green-news">
-          <div className="green-news-child">
-            <div className="green-news-title" id="illinois">
-              <h2>GREEN NEWS</h2>
-              <h4>A GREEN NEW DEAL FOR ILLINOIS TRANSPORTATION</h4>
+      {/* Edit divs when layout is implemented */}
+      <div className="page-title">
+        <h1>CLIMATE IMPACT BY TRANSPORTATION TYPE</h1>
+      </div>
+      <div className="page-content">
+        <div className="transportation-types-main">
+          <div className="title-list">
+            <div className="transportation-types-parent">
+              <div className="transportation-types">{TYPES}</div>
             </div>
-            <small>Last week while forest fires were raging in California, Governor Gavin Newsom issued an executive order declaring a goal of eliminating fossil fuels from the transportation sector.<br></br>
+          </div>
+          <div className="green-news">
+            <div className="green-news-child">
+              <div className="green-news-title" id="illinois">
+                <h2>GREEN NEWS</h2>
+                <h4>A GREEN NEW DEAL FOR ILLINOIS TRANSPORTATION</h4>
+              </div>
+              <div className="green-news-text">
+                <small>Last week while forest fires were raging in California, Governor Gavin Newsom issued an executive order declaring a goal of eliminating fossil fuels from the transportation sector.<br></br><br></br>
                     The order called for ending all sales of fossil fuel-powered vehicles by 2035 and calls on various California state agencies to formulate strategies and regulations to create a “just” transition away from fossil fuel reliance.</small>
-          </div>
-          <div className="green-news-child">
-            <div className="green-news-title" id="amazon">
-
-              <h2>GREEN NEWS</h2>
-              <h4>AMAZON UNVEILS ITS NEW ELECTRIC DELIVERY VANS BUILT BY RIVIAN</h4>
+              </div>
             </div>
-            <small>Amazon unveiled its first all-electric delivery van on Thursday. The vehicle, built by EV startup Rivian, will come with state-of-the-art technology, like sensing equipment and an advanced driver-assist system. <br></br>
+            <div className="green-news-child">
+              <div className="green-news-title" id="amazon">
+                <h2>GREEN NEWS</h2>
+                <h4>AMAZON UNVEILS ITS NEW ELECTRIC DELIVERY VANS BUILT BY RIVIAN</h4>
+              </div>
+              <div className="green-news-text">
+                <small>Amazon unveiled its first all-electric delivery van on Thursday. The vehicle, built by EV startup Rivian, will come with state-of-the-art technology, like sensing equipment and an advanced driver-assist system. <br></br><br></br>
                     The e-commerce giant says it expects to have 10,000 vans on the road making deliveries “as early as 2022,” with a total fleet of 100,000 vehicles expected by 2030.</small>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   )
 }
