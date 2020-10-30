fba033dae54c3549a538a5099dee19922e02ecac
Nathan Wigen <nathan.s.wigen@hotmail.com>
2020-10-30 10:32:10 -0700

    add Footer and Layout boilerplate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './screens/Home/Home';
 import PoliciesInitiatives from './screens/PoliciesInitiatives/PoliciesInitiatives';
 import TransportationTypes from './screens/TransportationType/TransportationTypes';
 import Header from "./components/shared/Header/Header";
+import Footer from './components/shared/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/policies-and-initiatives' component={PoliciesInitiatives} />
         <Route exact path="/" component={Home} />
         <Route exact path="/header" component={Header} />
+        <Route exact path="/Footer" component={Footer} />
         <Route exact path='/transportation-types' component={TransportationTypes} />
       </Switch>
     </div>
