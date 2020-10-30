4ff285f5b02a8d567a729905f569dfaad315d15d
Daniel Michael <itzdanielmichael@gmail.com>
2020-10-30 00:12:45 -0400

    brought back assets folder

--- client/src/screens/PoliciesInitiatives/PoliciesInitiatives.jsx
+++ client/src/screens/PoliciesInitiatives/PoliciesInitiatives.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './PoliciesInitiatives.css'
 import PoliciesInitiativesStatic from '../../components/PoliciesInitiatives/PoliciesInitiativesStatic'
+
 function PoliciesInitiatives() {
   return (
   <>
