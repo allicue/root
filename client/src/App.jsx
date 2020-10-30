f841579a14148e58a13c5b987a44ce4b9ea2b5b4
Eddie <edward.f.didonato@gmail.com>
2020-10-30 09:58:19 -0400

    merge feature front-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,17 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Route, Switch, Link } from 'react-router-dom'
-import TransportationTypes from './screens/TransportationType/TransportationTypes'
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <Link to="/TransportationTypes">transportation</Link>
-      <Switch>
-        <Route exact path='/TransportationTypes' component={TransportationTypes} />
-      </Switch>
-=======
 import { Route, Switch } from 'react-router-dom'
 import PoliciesInitiatives from './screens/PoliciesInitiatives/PoliciesInitiatives'
+import TransportationTypes from './screens/TransportationType/TransportationTypes'
 import Header from "./components/shared/Header/Header"
-        
+
 function App() {
   return (
     <div>
       <Switch>
-       <Route exact path='/policies-and-initiatives' component={PoliciesInitiatives} />  
-       <Route exact path="/" component={Header} />
-      </Switch> 
->>>>>>> 530e5b7ee0f38c058f9603e537404c057c1bca97
+        <Route exact path='/policies-and-initiatives' component={PoliciesInitiatives} />
+        <Route exact path="/" component={Header} />
+        <Route exact path='/transportation-types' component={TransportationTypes} />
+      </Switch>
     </div>
   );
 }
