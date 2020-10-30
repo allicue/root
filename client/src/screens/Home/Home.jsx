195af26b5c29590a35fb72f59d652f86feab9f48
Allison <allisonquiroz89@gmail.com>
2020-10-30 12:27:27 -0400

    Flexbox layout implemented for link tiles

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Home.css";
 import Logo from './photos/ROOT_Logo_RevWhite.png';
 import CommunityImg from './photos/molly_profile_community_advocacy_1res.png';
+import Train from './photos/root_desktop_hi_res.png';
 
 function Home(props) {
   return (
@@ -18,28 +19,36 @@ function Home(props) {
         </div>
         <hr></hr>
         <nav className="homepage-header-navbar">
-          <button><p className="button-text">Plan Your Route</p></button>
-          <button><p className="button-text">Climate Impact By Transportation Type</p></button>
-          <button><p className="button-text">Transportation <br></br>Policies + Initiatives</p></button>
+          <button className="homepage-button"><p className="button-text">Plan Your Route</p></button>
+          <button className="homepage-button"><p className="button-text">Climate Impact By Transportation Type</p></button>
+          <button className="homepage-button"><p className="button-text">Transportation <br></br>Policies + Initiatives</p></button>
         </nav>
       </div>
 
       <div className="homepage-body">
-        <section className="policies-section">
-          <span className="policies-text">
-            <h3 className="policies-text-headline">TRANSPORTATION <br></br>POLICIES + INITIATIVES</h3>
-            <p>See what's happening in our list of locations!</p>
-          </span>
-        </section>
-        <section className="climate-impact-section">
-          <h3>CLIMATE IMPACT</h3>
-          <p>Your Transportation options ranked by carbon emissions.</p>
-        </section>
-        <section className="community-advocacy-section">
-          <div className="community-a"><h3>COMMUNITY + ADVOCACY</h3></div>
-          <div className="community-b"><img className="community-advocacy-img" src={CommunityImg} alt="community-advocacy"/></div>
-        </section>
+        <div className="row">
+          <div className="column">
+            <section className="policies-section">
+              <span className="policies-text">
+                <h3 className="policies-text-headline">TRANSPORTATION <br></br>POLICIES + INITIATIVES</h3>
+                <p>See what's happening in our list of locations!</p>
+              </span>
+            </section>
+            <section className="community-advocacy-section">
+              <div className="community-a"><h3>COMMUNITY + ADVOCACY</h3></div>
+              <div className="community-b"><img className="community-advocacy-img" src={CommunityImg} alt="community-advocacy"/></div>
+              </section>
+          </div>
+          <div className="column">
+            <section className="climate-impact-section">
+              <div><img className="train-image" alt="women-waiting-for-train" src={Train}/></div>
+              <h3 className="climate-impact-header">CLIMATE IMPACT</h3>
+              <p className="climate-impact-text">Your Transportation options ranked by carbon emissions.</p>
+            </section>
+          </div>
+        </div>
       </div>
+      
 
     </div>
   );
