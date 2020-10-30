cde68b5a4747c25a2ca32505b303e54e270ca96f
Eddie <edward.f.didonato@gmail.com>
2020-10-29 20:32:46 -0400

    add mask to mapped images

--- client/src/components/TransportationType/TransportationType.jsx
+++ client/src/components/TransportationType/TransportationType.jsx
@@ -7,7 +7,7 @@ export default function TransportationType(props) {
   return (
     <div className="transportation-type">
       <div className="title-container" style={{ background: `url(${props.imgURL})`, backgroundSize: "380px", backgroundPosition: "center" }}>
-        <h3 >{props.title}</h3>
+        <div className="h3-container"><h3>{props.title}</h3></div>
       </div>
       <div className="description-container">
         <p>{props.description}</p>
