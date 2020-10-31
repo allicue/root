1f0a0f1a8d25b07d15ebec4866f92285a176d8b7
Daniel Michael <itzdanielmichael@gmail.com>
2020-10-31 10:48:06 -0400

    finishe rror page

--- client/src/screens/Error/Error.jsx
+++ client/src/screens/Error/Error.jsx
@@ -7,9 +7,9 @@ function Error() {
     return (
       <>
         <StyledError>
-        <h1> 404  </h1>
-          <p>Sorry, {location.pathname.replace('/', '')} doesn't exist!</p>
+        <h1> Error 404</h1>
           <img src="https://i.imgur.com/q4DDzXT.png" alt="Sad Emoji" />
+          <p>Sorry, {location.pathname.replace('/', '')} doesn't exist!</p>
       <NavLink to='/'> Go Back </NavLink>
       </StyledError>
            </>
--- client/src/screens/Error/styledError.js
+++ client/src/screens/Error/styledError.js
@@ -8,17 +8,30 @@ color: #3d3d3d;
 font-size: 24px;
 font-weight: bold;
 text-align: center;
-margin: 36px 44px;
+margin: 36px auto;
 text-transform: uppercase;
 
 a {
   text-decoration: none;
-  color: #3d3d3d;
+  color: #749f5c;
+  font-size: 36px;
+  font-weight: normal;
+  font-family: 'Monsterrat', sans serif;
+}
+
+p {
+  color: #000000;
+  margin-bottom: 20px;  
+}
+
+a:hover{
+  text-shadow: 1px 1px #999;
 }
 
 img {
   width: 200px;
   margin: 0 auto;
+  padding: 50px;
 }
 `
 
