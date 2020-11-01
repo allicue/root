e4a15da1bd3b4b403537a10eff9a8dbfe228fea8
Allison <allisonquiroz89@gmail.com>
2020-11-01 15:41:35 -0500

    created columns for top page layout

--- client/src/screens/PlanRoute/PlanRoute.jsx
+++ client/src/screens/PlanRoute/PlanRoute.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import ClockIcon from '../../Assets/Icons/ROOT_Time.png';
 import LeafIcon from '../../Assets/Icons/ROOT_Leaf.png';
+// import LogoBanner from '../../components/shared/LogoBanner';
+import WorldImg from '../../Assets/Images/Hi-Res/Frame_16.png';
 import './PlanRoute.css';
 
 function PlanRoute(props) {
   return (
     <div className="plan-route-page">
       <div className="plan-form-container">
+        <div className="column-a">
+          {/* <LogoBanner /> */}
         <form className="plan-form">
           <input className="plan-input" placeholder="Current Location"></input>
           <input className="plan-input" placeholder="The Metropolitan Museum of Art (MET)"></input>
           <button className="plan-button">FIND MY ROUTE OPTIONS!</button>
-        </form>
+          </form>
+        </div>
+        <div className='column-b'><img className="world-image" src={WorldImg} alt="globe"/></div>
       </div>
       <hr className="plan-breakpoint"></hr>
       <div className="grid-view">
@@ -25,7 +31,7 @@ function PlanRoute(props) {
           </section>
           <section className="grid-text"><h2>See Green Ratings</h2></section>
         </div>
-        {/* <hr className="grid-breakpoint"></hr> */}
+        <div className="grid-breakpoint"></div>
         <div className="grid-section-b">
           <section className="grid-icons">
             <img className="clock-icon" src={ClockIcon} />
@@ -34,7 +40,7 @@ function PlanRoute(props) {
             <h2>View Time to Destination</h2>
           </section>
         </div>
-        {/* <hr className="grid-breakpoint"></hr> */}
+        <div className="grid-breakpoint"></div>
         <div className="grid-section-c">
           <section className="grid-icons">$$$</section>
           <section className="grid-text"><h2>View Potential Cost of Trip</h2></section>
