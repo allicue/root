b83f5ea41cd92efdb30d9e4506f51beb1d3fe28c
Daniel Michael <itzdanielmichael@gmail.com>
2020-11-02 20:56:19 -0500

    dear god help

---
+++ client/src/components/shared/Header/Burger.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Header from './Header';
+
+const StyledBurger = styled.div`
+  width: 2rem;
+  height: 2rem;
+  position: fixed;
+  top: 15px;
+  z-index: 9999;
+  display: none;
+  left: 0;
+  margin-left: 20px;
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column nowrap;
+  }
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background-color: #3d3d3d;
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    transform-origin: 1px;
+    transition: all 0.3s linear;
+  }
+`;
+
+const Burger = () => {
+  const [open, setOpen] = useState(false)
+  
+  return (
+    <>
+      <StyledBurger open={open} onClick={() => setOpen(!open)}>
+        <div />
+        <div />
+        <div />
+      </StyledBurger>
+      <Header open={open}/>
+    </>
+  )
+}
+export default Burger
--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -1,35 +1,60 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./Header.css"
+import styled from 'styled-components'
 
-function Header() {
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  li {
+    padding: 18px 10px;
+  }
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #e8e4d9;
+    position: fixed;
+    transform: ${({ open }) => open ? 'translateY(0)' : 'translateY(-100%)'};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 100vw;
+    padding-top: 3.5rem;
+    transition: transform 0.4s ease-in-out;
+  
+    li {
+      margin: 0 auto;
+      
+    }
+  }
+`;
+
+function Header({ open }) {
   return (
     <header className="header">
-    <div className="menu">
+     <Ul open={open}>
       <div className="header-home">
-        <Link to="/" className="link-home">HOME</Link>
+      <li>  <Link to="/" className="link-home">HOME</Link> </li>
       </div>
       <div className="header-plan">
-      <Link to="/plan-your-route" className="link-plan">PLAN YOUR ROUTE</Link>
+      <li>  <Link to="/plan-your-route" className="link-plan">PLAN YOUR ROUTE</Link> </li>
       </div>
       <div className="header-transport">
-      <Link to="/transportation-types" className="link-transport">CLIMATE IMPACT BY <br></br> TRANSPORTATION TYPE</Link>
+    <li>  <Link to="/transportation-types" className="link-transport">CLIMATE IMPACT BY <br></br> TRANSPORTATION TYPE</Link> </li>
       </div>
       <div>
         <img className="header-image" src="https://i.imgur.com/thepnzF.png" alt="user profile" />
       </div>
       <div className="header-polices">
-      <Link to="/policies-and-initiatives" className="link-polices">POLICIES + INITIATIVES</Link>
+     <li> <Link to="/policies-and-initiatives" className="link-polices">POLICIES + INITIATIVES</Link> </li>
       </div>
       <div className="header-community">
-        <Link to="/" className="link-community">COMMUNITY + <br></br> ADVOCACY</Link>
+    <li>    <Link to="/" className="link-community">COMMUNITY + <br></br> ADVOCACY</Link> </li>
       </div>
       <div className="header-account">
-        <Link to="create-your-account" className="link-account">ACCOUNT</Link>
+     <li>  <Link to="create-your-account" className="link-account">ACCOUNT</Link> </li>
         </div> 
-      </div>
-      <input class="menu-btn" type="checkbox" id="menu-btn" />
-            <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+ </Ul>
     </header>
   )
 }
