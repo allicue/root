9ffb6e294ff908fc0394809f57cf469c9fa6d53f
Daniel Michael <itzdanielmichael@gmail.com>
2020-11-04 14:53:20 -0500

    sad page

---
+++ client/src/screens/SadToSeeYouGo/:(.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import StyledError from "./Sad";
+import Layout from "../../components/shared/Layout/Layout";
+import LogoBanner from "../../components/shared/LogoBanner/LogoBanner";
+
+function Sad() {
+  return (
+    <Layout>
+      <LogoBanner title="Manage Your Account" />
+      <StyledError>
+        <img src="https://i.imgur.com/q4DDzXT.png" alt="Sad Emoji" />
+        <p className="sad">We’re sad to see you go!</p>
+        <p className="although">Although you can’t engage with our commmunity <br /> anymore, you still have access to our ROOT <br /> <span>tools and resources!</span></p>
+      </StyledError>
+    </Layout>
+  );
+}
+
+export default Sad;
