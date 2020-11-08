b827c2bb7effba89fdb30156bbb67cbfd00c2d49
Daniel Michael <itzdanielmichael@gmail.com>
2020-11-08 16:00:56 -0500

    nvm just looked at phone has to be 20px

--- client/src/components/shared/Header/Ul.js
+++ client/src/components/shared/Header/Ul.js
@@ -25,14 +25,13 @@ const Ul = styled.ul`
     width: 100vw;
     padding-top: 3.5rem;
     padding-bottom: 100px;
-    
+
     li {
       margin: 0 auto;
       text-transform: capitalize;
       font-weight: 500;
       padding: 18px 10px;
     }
-    
   }
 `;
 
