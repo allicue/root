c130edd20e0a21c96352ba504c74f1bc97fedee4
Eddie <edward.f.didonato@gmail.com>
2020-11-07 20:59:36 -0500

    remove warnings

--- client/src/components/TransportationType/TransportationType.jsx
+++ client/src/components/TransportationType/TransportationType.jsx
@@ -5,16 +5,15 @@ import './TransportationType.css';
 export default function TransportationType(props) {
 
   const TitleContainer = styled.div`
-  .title-container {
+
     text-align: center;
     min-width: 300px;
     background: url(${props.imgURL});
     background-size: 130%;
     background-position: center;
-  }
+  
 
   @media screen and (max-width:500px) {
-    .title-container {
       min-width: 60px;
       height: 60px;
       width: 60px;
@@ -26,9 +25,7 @@ export default function TransportationType(props) {
   return (
     <div className="transportation-type">
       <TitleContainer>
-      <div className="title-container">
         <div className="h3-container"><h3 className="type-title">{props.title.toUpperCase()}</h3></div>
-      </div>
       </TitleContainer>
       <div className="title-plus-description">
         <h3 className="mobile-title" >{props.title}</h3>
