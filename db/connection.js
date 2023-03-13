f8b745dcdc7e769c0fa5b2bdbd9daa6c182e6bbd
Daniel <itzdanielmichael@gmail.com>
2023-03-13 15:59:01 -0400

    add dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/ROOTDB'
+let MONGODB_URI =
+  process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/ROOTDB';
 
 mongoose
-    .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
-    .then(() => console.log('Successfully connected to MongoDB.'))
-    .catch(error => console.error('Connection error', error.message))
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log('Successfully connected to MongoDB.'))
+  .catch((error) => console.error('Connection error', error.message));
 
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
